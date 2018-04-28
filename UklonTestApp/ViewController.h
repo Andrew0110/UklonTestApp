02674ff45125bfceb4572330cc71c1ded2859852
Andriy Roshchin <andriy.roshchin@gmail.com>
2018-04-28 09:39:20 +0300

    Initial Commit

---
+++ UklonTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UklonTestApp
+//
+//  Created by Andrew on 28.04.2018.
+//  Copyright © 2018 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UklonTestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UklonTestApp
+//
+//  Created by Andrew on 28.04.2018.
+//  Copyright © 2018 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
